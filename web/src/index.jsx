66ed0cdc1ad1f69813d3c860c40f9f68ef35b6fb
Balazs Komaromi <balazs.komaromi.photo@gmail.com>
2022-09-28 09:31:22 +0200

    Fix frontend test execution

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import GlobalStyle from './styles/GlobalStyle';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import GlobalStyle from "./styles/GlobalStyle";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <>
     <GlobalStyle />
     <App />
-  </>,
-  document.getElementById('root'),
+  </>
 );
